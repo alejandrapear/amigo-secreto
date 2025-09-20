024a3b3497686cf51675f6dba9c66a8f258dd452
Alejandra <alecitapear@gmail.com>
2025-09-20 14:03:10 -0400

    juego amigo secreto

---
+++ app.js
@@ -0,0 +1,78 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombreAmigo = [];
+let listaAmigos = [];
+
+function asignarTextoElemento(elemento,texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    
+}
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+    console.log(nombreAmigo);
+    limpiarCaja ();    
+    
+    if(nombreAmigo === "") {
+        alert("Debes ingresar un nombre");
+        return;    
+    }
+    listaAmigos.push(nombreAmigo);
+    actualizarListaAmigos();
+}
+
+ function limpiarCaja() {
+   document.querySelector("#amigo").value = "";
+ }
+
+        //asignarTextoElemento("ul.name-list",nombreAmigo)
+function actualizarListaAmigos() {
+
+
+    listaAmigos.forEach(function(nombreAmigo) {
+        let elemento = document.createElement("li");
+        elemento.textContent = nombreAmigo;
+        elemento.classList.add("listaAmigos");
+        
+    })
+    
+        console.log (listaAmigos);
+          
+
+}
+
+function sortearAmigo() {
+    let numeroAmigo = Math.floor(Math.random()*listaAmigos.length);
+    let amigoSecreto = listaAmigos[numeroAmigo];
+        if (listaAmigos.length >= 1) {
+            asignarTextoElemento("ul.result-list",amigoSecreto);
+        }
+        else { 
+            asignarTextoElemento("ul.result-list","");
+        }
+        console.log(amigoSecreto);    
+
+    if(listaAmigos.length === 0) {
+        alert("No hay amigos para sortear")
+    }
+    if(listaAmigos.length === 1) {
+        alert("Solo hay un amigo, añade todos los que puedas!!")
+        return;
+    } 
+
+}
+document.getElementById("sorteo").removeAttribute("disabled")
+
+asignarTextoElemento("h2","¡Añade a tus amigos para comenzar!!!!");
+
+//let parrafo = document.querySelector("p");
+//parrafo.innerHTML = "escribe el nombre de tus amigos"
+//introducir nombre de amigo
+//añadir nombres de amigo
+//enlistar los nombres añadidos
+//alertar de espacio vacio
+//alertar si se repite el nombre
+//si se repite el nombre añadir una caracteristica extra
+//sortear amigos
+//reiniciar juego
+  
